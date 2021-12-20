08e4c09a648f1ea234c03253bdd2df7a0f21d7a2
Adityachess <89146150+Adityachess@users.noreply.github.com>
2021-12-20 15:13:07 +0530

    Added:Extending Greeting Contoller to use Service Layer to get simple greeting message.

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -3,7 +3,9 @@ package com.bridgelabz.greetingapp.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.bridgelabz.greetingapp.model.Greeting;
+import com.bridgelabz.greetingapp.service.IGreetingService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,10 +18,9 @@ public class GreetingController {
 
 	/**
 	 * curl localhost:8080/greeting
-	 * 
 	 * @return={id =1 , content="hello world!}
-	 *             localhost:8080/greeting?name=Aditya @return= { id=2,
-	 *             content="hello Aditya!
+	 * localhost:8080/greeting?name=Aditya 
+	 * @return= { id=2,content="hello Aditya!
 	 */
 	@GetMapping(value = { "/greeting", "/greeting/", "/greeting/home" })
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
@@ -28,11 +29,24 @@ public class GreetingController {
 
 	/**
 	 * localhost:8080/greeting/Aditya
-	 * 
 	 * @return={id =1 , content="hello Aditya!}
 	 */
 	@GetMapping("greeting/{name}")
 	public Greeting greetings(@PathVariable String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@Autowired
+	private IGreetingService greetingService;
+
+	/**
+	 * localhost:8080/greeting/service
+	 * @return={id =1 , content="hello world!}
+	 */
+	@GetMapping("greeting/service")
+	public Greeting greeting() {
+		return greetingService.greetingMessage();
+
+	}
+
 }
