f08f41c4d46a469ff6fdefc4792ad72b916893f2
Adityachess <89146150+Adityachess@users.noreply.github.com>
2021-12-20 20:11:03 +0530

    Added:Ability for the Greeting App to give Greeting messages with Name.

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -2,12 +2,15 @@ package com.bridgelabz.greetingapp.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.bridgelabz.greetingapp.dto.UserDto;
 import com.bridgelabz.greetingapp.model.Greeting;
 import com.bridgelabz.greetingapp.service.IGreetingService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +20,6 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	/**
-	 * curl localhost:8080/greeting
 	 * @return={id =1 , content="hello world!}
 	 * localhost:8080/greeting?name=Aditya 
 	 * @return= { id=2,content="hello Aditya!
@@ -48,5 +50,11 @@ public class GreetingController {
 		return greetingService.greetingMessage();
 
 	}
+	
+	 @PostMapping("/greeting")
+     public String greetingMessage(@RequestBody UserDto userDto) {
+         return greetingService.greetingMessageByName(userDto);
+     }
+
 
 }
